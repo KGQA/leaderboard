c80a99a084b85f28771020c470a3073b25c70f8f
Artur-Galstyan <Artur-Galstyan@users.noreply.github.com>
2023-09-08 11:51:19 +0000

    deploy: 754cd8922a7d0b92e3f7441e259bb66b6fe83a39

---
+++ _app/immutable/chunks/paths.00192f0f.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_19sc9q1)==null?void 0:s.base)??"/leaderboard";var a;const t=((a=globalThis.__sveltekit_19sc9q1)==null?void 0:a.assets)??e;export{t as a,e as b};
--- _app/immutable/chunks/paths.e733ec2a.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1dt60t3)==null?void 0:s.base)??"/leaderboard";var t;const e=((t=globalThis.__sveltekit_1dt60t3)==null?void 0:t.assets)??a;export{e as a,a as b};
